1eb5484302bf3a2a60601c3e88494a8e2b77551d
Xeres <xeres666@googlemail.com>
2017-12-03 16:56:30 +0100

    day 3, part 2

--- day03.py
+++ day03.py
@@ -70,3 +70,78 @@ for test in tests:
         print "{} => {}".format(test, spiral_manhattan_distance(test))
 
 
+print ""
+print "part 2: spiral sum"
+
+
+tests = [
+    {'in': 1, 'out': 2},
+    {'in': 12, 'out': 23},
+    {'in': 23, 'out': 25},
+    347991
+]
+
+
+def spiral_sum(target_value):
+
+    def sum_adjacent(_dict, x, y):
+        v = []
+        v.append(_dict.get("{},{}".format(x + 1, y), 0))
+        v.append(_dict.get("{},{}".format(x - 1, y), 0))
+        v.append(_dict.get("{},{}".format(x, y + 1), 0))
+        v.append(_dict.get("{},{}".format(x, y - 1), 0))
+        v.append(_dict.get("{},{}".format(x + 1, y + 1), 0))
+        v.append(_dict.get("{},{}".format(x - 1, y - 1), 0))
+        v.append(_dict.get("{},{}".format(x + 1, y - 1), 0))
+        v.append(_dict.get("{},{}".format(x - 1, y + 1), 0))
+        return reduce(lambda x, y: x + y, v)
+
+    value = 1
+    x = 0
+    y = 0
+    ring_nr = 0
+    number_per_ring = 8
+    number_this_ring = 0
+    fieldValues = {}
+    fieldValues["0,0"] = 1
+    directions = {'r': [1, 0], 'u': [0, 1], 'l': [-1, 0], 'd': [0, -1]}
+    direction = 'r'
+    while value <= target_value:
+        d = directions.get(direction)
+        x += d[0]
+        y += d[1]
+        value += 1
+
+        thisfield = sum_adjacent(fieldValues, x, y)
+
+        if thisfield > target_value:
+            return thisfield
+
+        fieldValues["{},{}".format(x, y)] = thisfield
+
+        number_this_ring += 1
+        if number_this_ring > ring_nr * number_per_ring:
+            number_this_ring = 1
+            ring_nr += 1
+        if number_this_ring >= ring_nr * number_per_ring * 0.75:
+            direction = 'r'
+        elif number_this_ring >= ring_nr * number_per_ring * 0.50:
+            direction = 'd'
+        elif number_this_ring >= ring_nr * number_per_ring * 0.25:
+            direction = 'l'
+        else:
+            direction = 'u'
+    return value
+
+
+for test in tests:
+    if isinstance(test, dict):
+        in_value = test.get('in')
+        out_value = test.get('out')
+        out_result = spiral_sum(in_value)
+        if out_value == out_result:
+            print "{} => {} - OK".format(in_value, out_result)
+        else:
+            print "{} => {} - FAIL ({})".format(in_value, out_result, out_value)
+    else:
+        print "{} => {}".format(test, spiral_sum(test))
