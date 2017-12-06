f01dd8def07de609794a672cb065c2b2f482f5c9
Xeres <xeres666@googlemail.com>
2017-12-06 23:39:36 +0100

    day 6, part 2

--- day06.py
+++ day06.py
@@ -58,3 +58,56 @@ for test in tests:
         print "{} => {} - FAIL ({})".format(in_value, out_result, out_value)
 
 print "puzzle: {}".format(redistribute_count(list(inputs)))
+
+print ""
+print "part 2: count loop"
+
+tests = [
+    {'in': '0 2 7 0', 'out': 4},
+]
+
+
+def redistribute_loop_count(banks):
+
+    def highestValueIndex(items):
+        highest_index = 0
+        highest_value = items[0]
+        for index, value in enumerate(items):
+            if value > highest_value:
+                highest_value = value
+                highest_index = index
+        return highest_index
+
+    """
+        instead of the count, we calculate the
+    """
+    bank_length = len(banks)
+    count = 0
+    configuration = '-'.join(map(lambda v: str(v), banks))
+    configurations = set()
+    configuration_list = list()
+    while configuration not in configurations:
+        configurations.add(configuration)
+        configuration_list.append(configuration)
+        pos = highestValueIndex(banks)
+        blocks = banks[pos]
+        banks[pos] = 0
+        while blocks:
+            pos = (pos + 1) % bank_length
+            banks[pos] += 1
+            blocks -= 1
+        count += 1
+        configuration = '-'.join(map(lambda v: str(v), banks))
+    return len(configuration_list) - configuration_list.index(configuration)
+
+
+for test in tests:
+    in_value = test.get('in')
+    out_value = test.get('out')
+    out_result = redistribute_loop_count(map(lambda v: int(v), in_value.split()))
+    if out_value == out_result:
+        print "{} => {} - OK".format(in_value, out_result)
+    else:
+        print "{} => {} - FAIL ({})".format(in_value, out_result, out_value)
+
+print "puzzle: {}".format(redistribute_loop_count(list(inputs)))
